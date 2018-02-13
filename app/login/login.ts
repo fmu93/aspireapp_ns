867828ae6a54e034927b19007ef63db5b420a678
Paco Muro <fm@tadorea.com>
2018-02-13 14:03:33 +0100

    logout before login automatic

--- app/login/login.ts
+++ app/login/login.ts
@@ -24,31 +24,25 @@ export function onLoaded(args) {
 }
 
 export function signIn(args) {
-    if (BackendService.isLoggedIn()) {
         // Actually, the only way to be logged in at this point is after successful regisrtration
         // or successful login. Maybe a switch user does make sense.
         const oldUser = user.username;
         const promise = BackendService.logout()
         .then(() => {
-            dialogs.alert("user logged off: " + oldUser).then(() => {
-                //
+            const promise2 = BackendService.login(user)
+            .then(() => {
+                if (BackendService.isLoggedIn()) {
+                    frameModule.topmost().navigate("tabs/tabs-page");
+                } else {
+                    dialogs.alert("user not logged in: " + user.username);
+                }
+            }).catch((error: KinveyError) => {
+                dialogs.alert("Error logging in: " + user.username);
+                // console.log(error.stack);
         }).catch((error: Kinvey.BaseError) => {
             console.log("error loggin off");
         });
         });
-    } else {
-        const promise = BackendService.login(user)
-        .then(() => {
-            if (BackendService.isLoggedIn()) {
-                frameModule.topmost().navigate("tabs/tabs-page");
-            } else {
-                dialogs.alert("user not logged in: " + user.username);
-            }
-        }).catch((error: KinveyError) => {
-            dialogs.alert("Error logging in: " + user.username);
-            console.log(error.stack);
-        });
-    }
 }
 
 export function register(args) {
